638e099d393b1fe5af7094d844d2ca36314ae230
CAYLEB REGISTER <cayleb.register@gmail.com>
2023-06-23 18:58:03 -0400

    Adding lab6 file

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
-
-int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
+#include <stdlib.h>
+int search(int numbers[], int low, int high, int value) {
+    if (low > high) {
+		return -1;
+	}
+    int mid = (low + high) / 2;
+    if (numbers[mid] == value) {
+        return mid;
+    } else if (value < numbers[mid]) {
+        return search(numbers, low, mid - 1, value);
+    } else {
+        return search(numbers, mid + 1, high, value);
+    }
 }
+
 
 void printArray(int numbers[], int sz)
 {
